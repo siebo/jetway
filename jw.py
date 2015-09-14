e038031bc5540400ffabd94b284da5619c5889f5
David Siedband <david@zentraal.com>
2015-09-13 21:17:08 -0700

    parse the noun object (e.g. network name)

--- jw.py
+++ jw.py
@@ -5,5 +5,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-l","--list", help="list the available networks")
 parser.add_argument("-j","--join", help="join a network")
 parser.add_argument("-f","--forget", help="remove a preferred network")
+parser.add_argument("noun", help="target")
 args = parser.parse_args()
 print args
