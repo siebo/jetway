0f35edeffd388533a7111d7542928ff317b8bad2
David Siedband <david@zentraal.com>
2015-09-15 17:00:48 -0700

    added notes about command line options

--- jw.py
+++ jw.py
@@ -1,6 +1,17 @@
 import argparse
 import subprocess
 
+# networksetup -getairportpower <device name>
+# networksetup -setairportpower <device name> <on off>
+
+# networksetup -getairportnetwork <device name>
+# networksetup -setairportnetwork <device name> <network> [password]
+
+# networksetup -listpreferredwirelessnetworks <device name>
+# networksetup -addpreferredwirelessnetworkatindex <device name> <network> <index> <security type> [password]
+# networksetup -removepreferredwirelessnetwork <device name> <network>
+# networksetup -removeallpreferredwirelessnetworks <device name>
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-l","--list", help="list the available networks")
 parser.add_argument("-j","--join", help="join a network")
