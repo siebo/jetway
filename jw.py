645582ce321d76616747a8d145dbb45a74679953
David Siedband <david@zentraal.com>
2015-09-15 17:25:34 -0700

    syntax for scanning for networks

--- jw.py
+++ jw.py
@@ -12,6 +12,9 @@ import subprocess
 # networksetup -removepreferredwirelessnetwork <device name> <network>
 # networksetup -removeallpreferredwirelessnetworks <device name>
 
+# Scan for networks
+# /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-l","--list", help="list the available networks")
 parser.add_argument("-j","--join", help="join a network")
